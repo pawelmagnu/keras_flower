81a37fa3e3a7a48c8dfe7d0bc5d0e4f28ab158c9
pawel <pawel.magnu@gmail.com>
2021-06-03 00:30:07 +0200

    upto point 4

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-import numpy as np
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
 import pathlib
-# import tensorflow_datasets as tfds
+import numpy as np
 import matplotlib.pyplot as plt
-from tensorflow.keras import layers
-
+from tensorflow.keras import layers, models
 
 data_dir = './flowers/'
 data_dir = pathlib.Path(data_dir)
@@ -14,78 +12,18 @@ batch_size = 32
 img_height = 180
 img_width = 180
 image_count = len(list(data_dir.glob('*/*.jpg')))
-print(image_count)
-
-
-# train_ds = tf.keras.preprocessing.image_dataset_from_directory(
-#     data_dir,
-#     validation_split=0.2,
-#     subset="training",
-#     seed=123,
-#     image_size=(img_height, img_width),
-#     batch_size=batch_size)
-#
-# val_ds = tf.keras.preprocessing.image_dataset_from_directory(
-#     data_dir,
-#     validation_split=0.2,
-#     subset="validation",
-#     seed=123,
-#     image_size=(img_height, img_width),
-#     batch_size=batch_size)
-#
-# class_names = train_ds.class_names
-# print(class_names)
-#
-#
-# plt.figure(figsize=(10, 10))
-# for images, labels in train_ds.take(1):
-#     for i in range(9):
-#         ax = plt.subplot(3, 3, i + 1)
-#         plt.imshow(images[i].numpy().astype("uint8"))
-#         plt.title(class_names[labels[i]])
-#         plt.axis("off")
-# # plt.show()
-#
-# for image_batch, labels_batch in train_ds:
-#     print(image_batch.shape)
-#     print(labels_batch.shape)
-#     break
-#
-# normalization_layer = tf.keras.layers.experimental.preprocessing.Rescaling(1./255)
-#
-# normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
-# image_batch, labels_batch = next(iter(normalized_ds))
-# first_image = image_batch[0]
-# # Notice the pixels values are now in `[0,1]`.
-# print(np.min(first_image), np.max(first_image))
-#
+# print(image_count)
 AUTOTUNE = tf.data.AUTOTUNE
-#
-# train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
-# val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
-
-
-
+IMG_SIZE = 180
 
 list_ds = tf.data.Dataset.list_files(str(data_dir/'*/*.jpg'), shuffle=False)
 list_ds = list_ds.shuffle(image_count, reshuffle_each_iteration=False)
-
-# for f in list_ds.take(5):
-#     print(f.numpy())
-
-
 class_names = np.array(sorted([item.name for item in data_dir.glob('*') if item.name != "LICENSE.txt"]))
-# print(class_names)
-
-
 val_size = int(image_count * 0.2)
+
 train_ds = list_ds.skip(val_size)
 val_ds = list_ds.take(val_size)
 
-
-
-# print(tf.data.experimental.cardinality(train_ds).numpy())
-# print(tf.data.experimental.cardinality(val_ds).numpy())
 
 def get_label(file_path):
     # convert the path to a list of path components
@@ -95,11 +33,13 @@ def get_label(file_path):
     # Integer encode the label
     return tf.argmax(one_hot)
 
+
 def decode_img(img):
     # convert the compressed string to a 3D uint8 tensor
     img = tf.image.decode_jpeg(img, channels=3)
     # resize the image to the desired size
     return tf.image.resize(img, [img_height, img_width])
+
 
 def process_path(file_path):
     label = get_label(file_path)
@@ -108,13 +48,10 @@ def process_path(file_path):
     img = decode_img(img)
     return img, label
 
-# Set `num_parallel_calls` so multiple images are loaded/processed in parallel.
+
 train_ds = train_ds.map(process_path, num_parallel_calls=AUTOTUNE)
 val_ds = val_ds.map(process_path, num_parallel_calls=AUTOTUNE)
 
-# for image, label in train_ds.take(1):
-#     print("Image shape: ", image.numpy().shape)
-#     print("Label: ", label.numpy())
 
 def configure_for_performance(ds):
     ds = ds.cache()
@@ -123,42 +60,49 @@ def configure_for_performance(ds):
     ds = ds.prefetch(buffer_size=AUTOTUNE)
     return ds
 
+
 train_ds = configure_for_performance(train_ds)
 val_ds = configure_for_performance(val_ds)
-
 image_batch, label_batch = next(iter(train_ds))
 
-plt.figure(figsize=(10, 10))
-for i in range(9):
-    ax = plt.subplot(3, 3, i + 1)
-    plt.imshow(image_batch[i].numpy().astype("uint8"))
-    label = label_batch[i]
-    plt.title(class_names[label])
-    plt.axis("off")
-plt.show()
-
-num_classes = 5
-
-model = tf.keras.Sequential([
-  layers.experimental.preprocessing.Rescaling(1./255),
-  layers.Conv2D(32, 3, activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Conv2D(32, 3, activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Conv2D(32, 3, activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Flatten(),
-  layers.Dense(128, activation='relu'),
-  layers.Dense(num_classes)
+inputs = tf.keras.layers.InputLayer(input_shape=(180, 180,  3))
+resize_and_rescale = tf.keras.Sequential([
+    layers.experimental.preprocessing.Resizing(IMG_SIZE, IMG_SIZE),
+    layers.experimental.preprocessing.Rescaling(1./255)
+])
+data_augmentation = tf.keras.Sequential([
+    layers.experimental.preprocessing.RandomFlip("horizontal_and_vertical"),
+    layers.experimental.preprocessing.RandomRotation(0.2),
+    layers.experimental.preprocessing.RandomZoom(height_factor=(0.1, 0.8))
 ])
 
-model.compile(
-  optimizer='adam',
-  loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
-  metrics=['accuracy'])
+model = models.Sequential()
+model.add(inputs)
+model.add(resize_and_rescale)
+model.add(data_augmentation)
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(180,180,3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10))
 
-model.fit(
-  train_ds,
-  validation_data=val_ds,
-  epochs=10
-)
+model.summary()
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+history = model.fit(train_ds,
+                    epochs=10,
+                    validation_data=val_ds)
+
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.ylim([0.5, 1])
+plt.legend(loc='lower right')
+plt.show()
